0f0d844a912feb7b82be03bc3798dbae269b0f7e
MRasch1 <mrasch1102@gmail.com>
2024-04-12 12:54:45 +0200

    ICrudRepo

---
+++ ICE_Repository/Interfaces/ICrudRepo.cs
@@ -0,0 +1,21 @@
+﻿using ICE_Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ICE_Repository.Interfaces
+{
+    public interface ICrudRepo<Crud>
+    {
+        Crud Create(Crud crud);
+        List<Crud> GetAll();
+        Crud UpdateById(int id, Crud updatedUser);
+        Crud DeleteById(int id);
+    }
+    public interface IUserRepository : ICrudRepo<Users>
+    {
+    }
+}
